ee2335c5bc31c91b93b6b0274cb00c327f9aade6
“Juliet” <julietasiamah234@gmail.com>
2025-02-07 18:09:00 +0100

    commit message

--- Esame/src/App.jsx
+++ Esame/src/App.jsx
@@ -9,6 +9,9 @@ import MyFooter from "./components/MyFooter";
 function App() {
   return (
     <>
+      {/* Ho crearo 3 components e ho ricopiato la fetch come nel components Harrypotter per cambiare 
+    i poster generati. Non so se è il metodo più corretto. Mi era venuto in mente di clonare per 3 volte la components 
+    HarryPotter e usare le props ma ormani il tempo era scaduto perciò ho dovuto lasciare così. :) */}
       <TopBar />
 
       <HarryPotter tvshow="Tv Show" />
